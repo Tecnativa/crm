ce5003619fd0f7e26a8ea85f9bf42a681f2a801c
cubells <vicent@vcubells.net>
2016-11-07 10:55:19 +0100

    [MIG] crm_deduplicate_acl

--- crm_deduplicate_acl/__openerp__.py
+++ crm_deduplicate_acl/__openerp__.py
@@ -4,7 +4,7 @@
 {
     "name": "Deduplicate Contacts ACL",
     "summary": "Contact deduplication with fine-grained permission control",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Tools",
     "website": "http://www.antiun.com",
     "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
