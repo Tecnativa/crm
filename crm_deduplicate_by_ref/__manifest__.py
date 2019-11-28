b24cae34a33c6401ca5f44eb11677f97ece015b1
david <david.vidal@tecnativa.com>
2019-11-28 14:24:14 +0100

    [MIG] crm_deduplicate_by_ref -> partner_deduplicate_by_ref: Migration to 12.0

--- crm_deduplicate_by_ref/__manifest__.py
+++ crm_deduplicate_by_ref/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Deduplicate Contacts by reference",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Tools",
     "website": "https://www.github.com/OCA/crm",
     "author": "Tecnativa, "
@@ -10,7 +10,7 @@
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "crm_deduplicate_acl",
+        "partner_deduplicate_acl",
     ],
     "data": [
         'wizards/partner_merge_view.xml',
